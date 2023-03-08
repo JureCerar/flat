2c40060899c71efc596f307a987d8dfcb7fe3959
JureCerar <jure.cerar92@gmail.com>
2023-03-09 00:08:34 +0100

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,4 @@
-<<<<<<< HEAD
 from setuptools import find_packages, setup
-
-setup(
-  name='flat',
-  packages=find_packages(),
-  version='0.0.0',
-  description='',
-  author='Jure Cerar',
-  license='GNU GPL 3.0',
-  install_requires=[],
-  setup_requires=['pytest-runner'],
-  tests_require=['pytest'],
-  test_suite='tests',
-)
-=======
-#!/usr/bin/env python
-
-from distutils.core import setup
 
 try:
   from flat import __version__
@@ -26,14 +8,11 @@ except:
 
 setup(
   name='flat',
+  packages=find_packages(),
   version=__version__,
+  license='GNU GPL 3.0',
   description='PyMol Script Library/Collection/Repository',
-  author='',
-  author_email='',
+  author='Jure Cerar',
   url='',
-  packages=['flat'],
-  package_dir='',
-  package_data='',
   requires=['pymol'],
 )
->>>>>>> 88f069c7cace1fd1d18c6d01a83bba514d9292da
