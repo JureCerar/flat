93eeedae42c83946c095d47be206a04fd317ca53
JureCerar <37686734+JureCerar@users.noreply.github.com>
2023-07-28 15:05:43 +0200

    Started on colored fasta.

--- flat/fileio.py
+++ flat/fileio.py
@@ -162,10 +162,69 @@ def load_ndx(file, quiet=0, *, _self=cmd):
             cmd.select(name, f"({name}) | {buffer}", quiet=1)
 
     return
-
-
-
-
-
-
-
+    
+# @cmd.extend
+# def save_fasta( filename, selection='(all)', invert=0, gapped=1, quiet=1 ):
+#     '''
+#     Synopsis
+#         saveFasta filename [, selection [, invert [, gapped]]]
+#     Desciption:
+#         Save a html file with colored (by C-alpha atoms) fasta sequence.
+#     Source:
+#         save_colored_fasta(): https://github.com/speleo3/pymol-psico/blob/master/psico/fasta.py
+#     Arguments:
+#         filename    ...
+#         selection   Object selection (default=all).
+#         invert      Invert foreground and background colors (default=0)
+#         gapped      ...
+#     Example
+#         saveFasta fasta.html
+#     '''
+#     from pymol.exporting import _resn_to_aa as one_letter
+#     from pymol import Scratch_Storage
+#     one_letter.update( {'CYS2':'C','ASPP':'D','GLUP':'E','HSE':'H','HSD':'H','HSP':'H','LSN':'K'} )
+#     gapped, invert= int(gapped), int(invert)
+#     selection = '(%s) and guide' % (selection)
+#     html = []
+#     stored = Scratch_Storage()
+#     def callback(resv, resn, color):
+#         if stored.resv is None:
+#             stored.resv = resv - (resv % 70)
+#         if gapped:
+#             while stored.resv+1 < resv:
+#                 callback(stored.resv+1, '-', 25)
+#         stored.resv += 1
+#         if stored.resv % 70 == 1:
+#             html.append(('</span>\n<br>%4d <span>' % (resv)).replace(' ', '&nbsp;'))
+#             stored.color = None
+#         rgb = cmd.get_color_tuple(color)
+#         rgb = [ int(x*255) for x in rgb ]
+#         color = '#%02x%02x%02x' % (rgb[0],rgb[1],rgb[2])
+#         if not invert:
+#             # https://stackoverflow.com/questions/11867545    
+#             textColor =  ['white','black'][ round((rgb[0]*299+rgb[1]*587+rgb[2]*114)/1000) > 125 ]
+#             backgroundColor = color
+#         else:
+#             textColor = color
+#             backgroundColor='white'
+#         aa = one_letter.get(resn,'-')
+#         if color != stored.color:
+#             html.append('</span><span style="color:'+textColor+';background-color:'+backgroundColor+'">')
+#             stored.color = color
+#         html.append(aa)
+#         if stored.resv % 10 == 0:
+#             html.append("</span> <span>")
+#     for obj in cmd.get_object_list(selection):
+#         for chain in cmd.get_chains('model %s and (%s)' % (obj, selection)):
+#             sele = 'model %s and chain "%s" and (%s)' % (obj, chain, selection)
+#             html.append('\n<br>&gt;%s_%s<span>' % (obj, chain))
+#             stored.resv = None if gapped else 0
+#             stored.color = None
+#             cmd.iterate(sele, 'callback(resv, resn, color)', space=locals())
+#             html.append('</span>')
+#     handle = open( filename, 'w')
+#     print( '<html><body style="font-family:consolas">', file=handle )
+#     print( ''.join(html), file=handle )
+#     print( '</body></html>', file=handle )
+#     handle.close()
+#     return 
