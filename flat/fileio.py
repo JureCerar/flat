ed75604d9c7e0c46d878bc3e137a3da13510abf1
JureCerar <jure.cerar92@gmail.com>
2023-07-28 21:53:59 +0200

    Updated fileio.

--- flat/fileio.py
+++ flat/fileio.py
@@ -1,3 +1,4 @@
+from pymol.exporting import _resn_to_aa as one_letter
 from pymol import cmd
 import collections
 import re
@@ -76,7 +77,8 @@ def load_topol(file, selection="(all)", *, _self=cmd):
             # nr, type, resnr, residue, atom, cgnr, pcharge, mass
             col = line.split()
             try:
-                index, pcharge, mass = int(col[0]), float(col[6]), float(col[7])
+                index, pcharge, mass = int(
+                    col[0]), float(col[6]), float(col[7])
                 atom_list[index] = [pcharge, mass]
             except:
                 print(f"Cannot parse line: '{line}'")
@@ -113,7 +115,8 @@ def save_ndx(file="index.ndx", quiet=0, *, _self=cmd):
                 print(f"Saving group: '{name}' len(group atoms)")
             print(f"[ {name} ]", file=handle)
             for i in range(0, len(group), nlines):
-                print(" ".join(f"{x:5}" for x in group[i:i+nlines]), file=handle)
+                print(
+                    " ".join(f"{x:5}" for x in group[i:i+nlines]), file=handle)
 
     return
 
@@ -135,10 +138,10 @@ def load_ndx(file, quiet=0, *, _self=cmd):
         line = line.split(";")[0]
         key = _get_keyword(line)
         if key:
-           current = key
-           groups[current] = []
+            current = key
+            groups[current] = []
         else:
-           groups[current] += [int(i) for i in line.split()]
+            groups[current] += [int(i) for i in line.split()]
 
     for name, group in groups.items():
         # Pymol does not like '&' and '|' characters and reserved keywords
@@ -153,7 +156,7 @@ def load_ndx(file, quiet=0, *, _self=cmd):
 
         if not quiet:
             print("Loading group: '{}' ({} atoms)".format(name, len(group)))
-        
+
         # Pymol does not like long selection strings so we select the group iteratively
         every = 15
         cmd.select(f"({name})", f"index {group[0]}")
@@ -162,69 +165,67 @@ def load_ndx(file, quiet=0, *, _self=cmd):
             cmd.select(name, f"({name}) | {buffer}", quiet=1)
 
     return
-    
-# @cmd.extend
-# def save_fasta( filename, selection='(all)', invert=0, gapped=1, quiet=1 ):
-#     '''
-#     Synopsis
-#         saveFasta filename [, selection [, invert [, gapped]]]
-#     Desciption:
-#         Save a html file with colored (by C-alpha atoms) fasta sequence.
-#     Source:
-#         save_colored_fasta(): https://github.com/speleo3/pymol-psico/blob/master/psico/fasta.py
-#     Arguments:
-#         filename    ...
-#         selection   Object selection (default=all).
-#         invert      Invert foreground and background colors (default=0)
-#         gapped      ...
-#     Example
-#         saveFasta fasta.html
-#     '''
-#     from pymol.exporting import _resn_to_aa as one_letter
-#     from pymol import Scratch_Storage
-#     one_letter.update( {'CYS2':'C','ASPP':'D','GLUP':'E','HSE':'H','HSD':'H','HSP':'H','LSN':'K'} )
-#     gapped, invert= int(gapped), int(invert)
-#     selection = '(%s) and guide' % (selection)
-#     html = []
-#     stored = Scratch_Storage()
-#     def callback(resv, resn, color):
-#         if stored.resv is None:
-#             stored.resv = resv - (resv % 70)
-#         if gapped:
-#             while stored.resv+1 < resv:
-#                 callback(stored.resv+1, '-', 25)
-#         stored.resv += 1
-#         if stored.resv % 70 == 1:
-#             html.append(('</span>\n<br>%4d <span>' % (resv)).replace(' ', '&nbsp;'))
-#             stored.color = None
-#         rgb = cmd.get_color_tuple(color)
-#         rgb = [ int(x*255) for x in rgb ]
-#         color = '#%02x%02x%02x' % (rgb[0],rgb[1],rgb[2])
-#         if not invert:
-#             # https://stackoverflow.com/questions/11867545    
-#             textColor =  ['white','black'][ round((rgb[0]*299+rgb[1]*587+rgb[2]*114)/1000) > 125 ]
-#             backgroundColor = color
-#         else:
-#             textColor = color
-#             backgroundColor='white'
-#         aa = one_letter.get(resn,'-')
-#         if color != stored.color:
-#             html.append('</span><span style="color:'+textColor+';background-color:'+backgroundColor+'">')
-#             stored.color = color
-#         html.append(aa)
-#         if stored.resv % 10 == 0:
-#             html.append("</span> <span>")
-#     for obj in cmd.get_object_list(selection):
-#         for chain in cmd.get_chains('model %s and (%s)' % (obj, selection)):
-#             sele = 'model %s and chain "%s" and (%s)' % (obj, chain, selection)
-#             html.append('\n<br>&gt;%s_%s<span>' % (obj, chain))
-#             stored.resv = None if gapped else 0
-#             stored.color = None
-#             cmd.iterate(sele, 'callback(resv, resn, color)', space=locals())
-#             html.append('</span>')
-#     handle = open( filename, 'w')
-#     print( '<html><body style="font-family:consolas">', file=handle )
-#     print( ''.join(html), file=handle )
-#     print( '</body></html>', file=handle )
-#     handle.close()
-#     return 
+
+
+@cmd.extend
+def save_colored_fasta(file, selection="all", invert=0, *, _self=cmd):
+    """
+    DESCRIPTION
+        Save a HTML file with colored (by C-alpha atoms) fasta sequence.
+    USAGE
+        save_colored_fasta file [, selection [, invert ]]]
+    """
+    # See: https://github.com/speleo3/pymol-psico/blob/master/psico/fasta.py
+    from pymol import Scratch_Storage
+    invert = int(invert)
+    selection = f"({selection}) and guide"
+
+    html = []
+    stored = Scratch_Storage()
+
+    def callback(resv, resn, color):
+        """ Translate to residue to HTML."""
+        if stored.resv is None:
+            stored.resv = resv - (resv % 70)
+        while stored.resv+1 < resv:
+            callback(stored.resv+1, None, 222)
+        stored.resv += 1
+        if stored.resv % 70 == 1:
+            html.append("<br>")
+
+        rgb = cmd.get_color_tuple(color)
+        rgb = [int(x*255) for x in rgb]
+        aa = one_letter.get(resn, "?") if resn else "-"
+
+        if not resn:
+            color = "#000000"
+            background = ""
+        elif invert:
+            cs = round((rgb[0] * 299 + rgb[1] * 587 + rgb[2] * 114) / 1000)
+            color = "black" if cs > 125 else "white" 
+            background = "background-color:#{:02x}{:02x}{:02x}".format(*rgb)
+        else:
+            color = "#{:02x}{:02x}{:02x}".format(*rgb)
+            background = ""
+
+        html.append(
+            f"<span style=color:{color};{background}>{aa}</span>"
+        )
+        if stored.resv % 10 == 0:
+            html.append("<span> </span>")
+        return
+
+    for obj in cmd.get_object_list(selection):
+        for chain in cmd.get_chains(f"model {obj} and ({selection})"):
+            sele = f"m. {obj} & c. '{chain}' and ({selection})"
+            html.append(f"&gt;{obj}|{chain}")
+            stored.resv = 0
+            cmd.iterate(sele, "callback(resv, resn, color)", space=locals())
+            html.append("<br>")
+
+    with open(file, "w") as handle:
+        print("<html><body style='font-family:consolas'>", file=handle)
+        print("".join(html), file=handle)
+        print("</body></html>", file=handle)
+
+    return
