1e5e34ac92b505e444bf3fa5b260401b9cbd2087
Jure Cerar <jure.cerar92@gmail.com>
2024-06-09 23:53:58 +0200

    Improvements to propka interface

--- flat/interface.py
+++ flat/interface.py
@@ -18,7 +18,7 @@ import tempfile
 import os
 
 @cmd.extend
-def propka(selection="all", state=0, file=None, vis=1, optargs=[]):
+def propka(selection="all", state=0, filename=None, vis=1, optargs=[], *, _self=cmd):
     """
     DESCRIPTION
         Predicts the pKa values of ionizable groups in proteins and
@@ -28,7 +28,7 @@ def propka(selection="all", state=0, file=None, vis=1, optargs=[]):
     ARGUMENTS
         selection = str: Atom selection. {default: all}
         state = int: Object state (0 for all states). {default: 0}
-        file = str: Save PROPKA output to file. {default: None}
+        filename = str: Save PROPKA output to file. {default: None}
         vis = bool: Visualize residue pKa values. {default: True}
         optargs = list: Optional arguments to be passed to PROPKA. {default: []}
     """
@@ -39,7 +39,7 @@ def propka(selection="all", state=0, file=None, vis=1, optargs=[]):
     state, vis = int(state), int(vis)
 
     temp = os.path.join(tempfile.mkdtemp(), "temp.pdb")
-    cmd.save(temp, selection, state)
+    _self.save(temp, selection, state)
 
     try:
         pka = pk.single(temp, optargs, None, False)
@@ -47,12 +47,12 @@ def propka(selection="all", state=0, file=None, vis=1, optargs=[]):
         pass
 
     if vis:
-        cmd.show(
+        _self.show(
             "lines",
             f"((byres ({selection})) & (sc.|(n. CA|n. N&r. PRO)))"
         )
-        cmd.set("label_size", 12.0)
-        cmd.set("sphere_scale", 0.25, "(all)")
+        _self.set("label_size", 12.0)
+        _self.set("sphere_scale", 0.25, "(all)")
         for group in pka.conformations["AVR"].groups:
             if group.residue_type in ["ASP", "GLU", "HIS", "TYRF", "LYS", "ARG", "N+", "C-"]:
                 chain_id = "" if group.atom.chain_id == "_" else group.atom.chain_id
@@ -63,14 +63,21 @@ def propka(selection="all", state=0, file=None, vis=1, optargs=[]):
                     group.atom.res_num,
                     group.atom.name,
                 )
-                cmd.label(atom, label)
-                cmd.show("spheres", atom)
+                _self.label(atom, label)
+                _self.show("spheres", atom)
 
-    if file:
-        pka.write_pka(file)
+    if filename:
+        pka.write_pka(filename)
 
     return pka
 
+
 @cmd.extend
-def anarci(selection="all"):
-    return
+def anarci(selection="all", *, _self=cmd):
+    raise NotImplemented()
+
+# Autocomplete
+cmd.auto_arg[0].update({
+    "propka": cmd.auto_arg[0]["zoom"],
+    "anarci": cmd.auto_arg[0]["zoom"],
+})
